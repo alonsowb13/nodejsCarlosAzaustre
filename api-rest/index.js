3a3ea26bd95341f24ebc4a3e924b27a9b19c5c7a
Alonso Salcido <alonsosalcido13@gmail.com>
2017-08-25 00:04:44 -0600

    adding body-parser and nodemon in dev dependencies

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,9 +1,18 @@
 'use strict' //esto es para utilizar los nuevos tipos de variables y el uso de ECMA6
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.listen(3000, ()=>{
-    console.log("API REST corriendo en localhost:3000")
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())//poder aceptar peticiones en formato json
+
+app.listen(port, ()=>{
+   console.log(`API REST corriendo en localhost:${port}`)
 })
+
+//bodyparser parsea la peticion que se hace atraves del protocolo http es un middleware
+//nodemon se instala para usar un livereload en el server y se crea en el dev dependencies de package json
