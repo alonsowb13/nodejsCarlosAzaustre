984892bbad54b0f9a97841f209894fdf4e6b5e9d
alonsowb13 <alonsosalcido0318@gmail.com>
2017-11-05 23:20:04 -0800

    refactorizing code in APIREST

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,80 +1,15 @@
 'use strict' //esto es para utilizar los nuevos tipos de variables y el uso de ECMA6
-
-const express = require('express')
-const bodyParser = require('body-parser')
-
-const app = express()
-
-const port = process.env.PORT || 3001
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())//poder aceptar peticiones en formato json
+const config = require('./config')
+const app = require('./app')
 const mongoose = require('mongoose')
 
-const Product = require('./models/product')
-
-//mostrar un mensaje con un ruta del elemento
-/*app.get('/hola', (req, res)=>{
-    res.send({message: 'Hola Mundo'});
-})*/
-//pasar parametros por el la url
-/*app.get('/hola/:name', (req, res)=>{
-    res.send({message: `Hola ${req.params.name}`});
-})*/
-
-//User JSON formatter en la chrome store
-
-app.get('/api/product', (req, res) => {
-    Product.find({}, (err, products)=>{
-        if(err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
-            if(!products) return res.status(404).send({message: 'No existen productos'})
-                res.status(200).send({products})
-    })
-})
-
-app.get('/api/product/:productId', (req, res)=>{
-    let productId = req.params.productId
-
-    Product.findById(productId, (err, product)=>{
-        if(err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
-            if(!product) return res.status(404).send({message: 'El producto no existe'})
-                res.status(200).send({product})
-    });
-})
-
-app.post('/api/product', (req, res)=>{
-    console.log('POST /api/product')
-    console.log(req.body)
-
-    let product = new Product()
-    product.name = req.body.name
-    product.picture = req.body.picture
-    product.price = req.body.price,
-    product.category = req.body.category
-    product.description = req.body.description 
-
-    product.save((err, productStored)=>{
-        if(err) res.status(500).send({message: `Error al salvar en la base de datos: ${err}`})
-        
-            res.status(200).send({product: productStored})
-    })
-})
-
-app.put('/api/product/:productId', (req, res)=>{
-
-})
-
-app.delete('/api/product/:productId', (req, res)=>{
-
-})
-
-mongoose.connect('mongodb://localhost:27017/shop', (err, res)=>{
+mongoose.connect(config.db, (err, res)=>{
     if(err){
         return console.log(`Error al conectar a la BD: ${err}`);
     }
     console.log('Conexion a la BD establecida...')
-    app.listen(port, ()=>{
-        console.log(`API REST corriendo en localhost:${port}`)
+    app.listen(config.port, ()=>{
+        console.log(`API REST corriendo en localhost:${config.port}`)
      })
 })
 
